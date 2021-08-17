473edfe5277a85304870cc73c820e837a491e9de
Tin Lai <oscar@tinyiu.com>
2021-08-17 19:04:49 +1000

    allow selecting transaction to delete via number and ignore option
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- rules/rule_remove_duplicates.py
+++ rules/rule_remove_duplicates.py
@@ -10,42 +10,37 @@ from miscs import search_keywords_in_text, get_transaction_owner
 from rules.base_rule import Rule, StopRuleProcessing
 
 
-# replace_schema = Schema({str: Or(str, [str])})
-
-# condition_schema = Schema({"field": str, "value": str})
-
-# search_keyword_schema = Schema(
-#     [
-#         Schema(
-#             {
-#                 "name": str,
-#                 Optional("num_of_token", default="ignore"): Or(str, int),
-#                 Optional("target", default="description"): str,
-#                 Optional("keyword", default=""): str,
-#                 Optional("stop", default=False): bool,
-#                 Optional("conditional"): [
-#                     Schema(
-#                         {
-#                             Optional("contain_keywords"): [condition_schema],
-#                             Optional("not_contain_keywords"): [condition_schema],
-#                             "replace": replace_schema,
-#                         }
-#                     )
-#                 ],
-#                 Optional("replace"): replace_schema,
-#             }
-#         )
-#     ]
-# )
+remove_duplicates_schema = Schema(
+    [
+        Schema(
+            [int],
+        )
+    ]
+)
 
 
 class RemoveDuplicates(Rule):
     def __init__(self, *args, **kwargs):
         super().__init__("remove_duplicates", *args, **kwargs)
-        # self._rule_config = config["rules"]["search_keyword"]
-        # self._rule_config = search_keyword_schema.validate(self._rule_config)
+        self._rule_config = config["rules"]["remove_duplicates"]
+        self._rule_config = remove_duplicates_schema.validate(self._rule_config)
+        self.ids_that_allow_duplicates = list(map(set, self._rule_config))
         self.df_transactions = None
         self.delete_master_id = set()
+        self.ignored_entries = []
+        import atexit
+
+        def exit_handler():
+            if len(self.ignored_entries) == 0:
+                return
+            print()
+            print("===============================================")
+            print("> The followings are ids that had been ignored.")
+            for entries in self.ignored_entries:
+                print(f"# [{entries[0][1]}] {entries[0][2]}")
+                print(f"- [{', '.join(e[0] for e in entries)}]")
+
+        atexit.register(exit_handler)
 
     @property
     def enable_by_default(self) -> bool:
@@ -63,34 +58,68 @@ class RemoveDuplicates(Rule):
         potential_duplicates = self.df_transactions[
             self.df_transactions.description.str.startswith(entry.description)
             & (self.df_transactions.date == entry.date)
-            & (self.df_transactions.type == entry.type)
+            & (
+                (self.df_transactions.source_name == entry.source_name)
+                | (self.df_transactions.destination_name == entry.destination_name)
+            )
+            # & (self.df_transactions.type == entry.type)
             & (
                 (self.df_transactions.amount.astype(float) - float(entry.amount)).abs()
-                < 0.01
+                < 0.001
             )
         ]
         assert len(potential_duplicates) >= 1, "Logic error?"
         assert int(entry.id) in set(potential_duplicates.id.astype(int)), "Logic error?"
         if len(potential_duplicates) > 1:
+
+            all_ids = set(potential_duplicates.id.astype(int))
+            if any(len(all_ids.difference(ids)) == 0 for ids in self.ids_that_allow_duplicates):
+                # these are in exceptions as specified by configs
+                return
+
             # remove self
             potential_duplicates = potential_duplicates[
                 potential_duplicates.id != entry.id
             ]
             print(f"==========================")
-            print(f" date: {entry.date}")
+            print(f"  date: {entry.date}")
             print(f" amount: ${float(entry.amount)}")
-            print(f" from: {entry.description}")
-            print(f"   id: {entry.id}")
-            print(f"  acc: {get_transaction_owner(entry, True)}")
+            print(f"   type: {entry.type}")
+
+            choices = [
+                (entry.id, get_transaction_owner(entry, True), entry.description)
+            ]
             for idx, row in potential_duplicates.iterrows():
-                print(
-                    f"     dupl: {row.description} ({row.id}: {get_transaction_owner(row, True)})"
+                choices.append(
+                    (row.id, get_transaction_owner(row, True), row.description)
                 )
+
+            choices = sorted(choices, key=lambda x: int(x[0]))
+            show_owner = not all(choices[0][1] for c in choices)
+            if not show_owner:
+                print(f"    acc: {choices[0][1]}")
+
+            for i, (id, owner, desc) in enumerate(choices):
+                owner_info = f"[{owner:>10}] " if show_owner else ""
+                print(f" [{i+1:>1}] {id:>4}: {owner_info}{desc}")
                 # import pprint
                 # pprint.pprint(row)
-            user_input = input(">> Remove all dupl? [y/N]")
-            if user_input.lower() != "y":
-                return
-            self.delete_master_id.add(entry.id)
-            for idx, row in potential_duplicates.iterrows():
-                self.pending_deletes.add(row.id)
+            while True:
+                user_input = input(f">> Which one to delete? [1..{len(choices)}/N] ")
+                if user_input.strip() == "":
+                    self.ignored_entries.append(choices)
+                    break
+                try:
+                    num = int(user_input)
+                    if not (1 <= num <= len(choices)):
+                        print(f"> Input must be between 1 and {len(choices)}")
+                        continue
+                    self.pending_deletes.add(choices[num - 1][0])
+                    break
+                except ValueError:
+                    print("> Input is not numeric")
+
+            for c in choices:
+                self.delete_master_id.add(c[0])
+
+            # for idx, row in potential_duplicates.iterrows():
